e5db78693b8e903a8ee6b9319ef8f1a1067007f7
Jon Poindexter <7112421+W5ALC@users.noreply.github.com>
2021-01-23 16:13:58 -0700

    Update convert_audio.sh
    changed to support .mp3 instead of .wav

--- convert_audio.sh
+++ convert_audio.sh
@@ -6,16 +6,16 @@
 if [ -z $1 ] || [ ! -f $1 ]
   then
    echo -e "\n Convert wav file to Asterisk/Allstar format\n"
-   echo -e "   convert_audio.sh <input file.wav> [output_filename]\n"
-   echo -e "   Input file must be .wav, output file is input filename.ul"
+   echo -e "   convert_audio.sh <input file.mp3> [output_filename]\n"
+   echo -e "   Input file must be .mp3, output file is input filename.ul"
    echo -e "   unless different second parameter filename is given.\n"
    exit
 fi
 
 extent=$(echo ${1,,} | cut -f 2 -d '.')
-if [ "$extent" != "wav" ]
+if [ "$extent" != "mp3" ]
   then
-   echo -e "\nInput file must be .wav\n"
+   echo -e "\nInput file must be .mp3\n"
    exit
 fi
 
@@ -26,7 +26,8 @@ if [ ! -z $2 ]
     outname="${1%.*}.ul"
 fi
 echo -e "\nConverting $1 to $(pwd)/$outname in Asterisk/Allstar format"
-sox -V2 $1 -r 8000 -c 1 -t ul $outname 
+#sox -V2 $1 -r 8000 -c 1 -t ul $outname 
+ffmpeg -i $1 -ar 8000 -ac 1 -ab 64 -f mulaw $outname -map 0:0 -map 0:0
 echo -e "\nCONVERSION COMPLETE\n\nUse Asterisk localplay or playback wihout the extent to play\n"
 echo -e "EXAMPLE - asterisk -rx \"rpt localplay <node> `pwd`/${outname%.*}\"\n" 
 
